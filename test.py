b953e783b0d44a8210ef078e684fb1411b8bc5a9
Tony Chou <tcheon8788@gmail.com>
2020-12-22 23:02:35 +0800

    init

---
+++ test.py
@@ -0,0 +1,56 @@
+import heapq
+import bs4 as bs
+import urllib.request
+import re
+import nltk
+
+
+def summarize_NLTK(article_text):
+    article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+    article_text = re.sub(r'\s+', ' ', article_text)
+
+    # Removing special characters and digits
+    formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
+    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+
+    sentence_list = nltk.sent_tokenize(article_text)
+
+    stopwords = nltk.corpus.stopwords.words('english')
+
+    word_frequencies = {}
+    for word in nltk.word_tokenize(formatted_article_text):
+        if word not in stopwords:
+            if word not in word_frequencies.keys():
+                word_frequencies[word] = 1
+            else:
+                word_frequencies[word] += 1
+
+    maximum_frequncy = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+
+    sentence_scores = {}
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_frequencies.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word]
+
+    summary_sentences = heapq.nlargest(
+        3, sentence_scores, key=sentence_scores.get)
+
+    return summary_sentences
+
+
+if __name__ == '__main__':
+    with open('test.txt', 'r', encoding="utf-8") as f:
+        article_text = f.read()
+
+    summary_sentences = summarize_NLTK(article_text)
+
+    for sentence in summary_sentences:
+        print(sentence)
